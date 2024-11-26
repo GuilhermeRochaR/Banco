b3095534cb2fe569208f831e7f6bda25273b61cf
Guilherme Rocha <luisguilherme.tecnologia@gmail.com>
2024-11-26 16:58:23 -0300

    remoção do index da raiz

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")  # Página inicial
+
+@app.route("/about")
+def about():
+    return render_template("about.html")  # Página "Sobre"
+
+@app.route("/api/data", methods=["GET"])
+def get_data():
+    return jsonify({"message": "Bem-vindo à API!", "status": "sucesso"})
+
+@app.route("/api/data", methods=["POST"])
+def post_data():
+    data = request.json
+    return jsonify({"message": "Dados recebidos!", "data": data}), 201
