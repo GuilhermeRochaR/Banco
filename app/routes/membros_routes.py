5c69af97ce98cea60df8a4e8e16cdce5de4c0dfa
Yan <yangarridodev@gmail.com>
2024-11-27 16:50:23 -0300

    validação membros

--- app/routes/membros_routes.py
+++ app/routes/membros_routes.py
@@ -1,34 +1,76 @@
-from flask import Blueprint, request, render_template, request, redirect
-from flask_login import login_required
+from flask import Blueprint, render_template, request, redirect, make_response, flash, session
 from models.membros import Membro
-from configs.extensions import db
+from flask_login import login_required, login_user, logout_user, current_user
+from validation.validation_utils import validate_login_data
 
 membros_router = Blueprint("membros_router", __name__, template_folder="templates")
 
 # Rota para listar todos os membros
 @membros_router.get("/")
 @login_required
-def get_all_membros():
-    membros = Membro.query.all()
-    return render_template("membros.html", membros=membros)
+def render_index():
+    membro_name = current_user.name
+    return render_template("membros.html", membro_name=membro_name)
 
-# Rota para criar um novo membro
-@membros_router.post("/membros")
-@login_required
-def create_membro():
-    data = request.json
-    novo_membro = Membro(
-        nome=data.get("nome"),
-        email=data.get("email"),
-        telefone=data.get("telefone"),
-        endereco=data.get("endereco"),
-        data_nascimento=data.get("data_nascimento"),
-        data_entrada=data.get("data_entrada"),
-    )
-    db.session.add(novo_membro)
-    db.session.commit()
-    return {
-        "id": novo_membro.id_membro,
-        "nome": novo_membro.nome,
-        "email": novo_membro.email
+@membros_router.get("/login")
+def index():
+    return render_template("login.html")
+
+@membros_router.get("/register")
+def render_register():
+    return render_template("cadastro.html")
+
+@membros_router.post("/register")
+def register():
+    nome = request.form["nome"]
+    email = request.form["email"]
+    telefone = request.form["telefone"]
+    endereco = request.form["endereço"]
+    senha = request.form["senha"]
+    confirma_senha = request.form["confirmarsenha"]
+    datadenascimento = request.form
+    ["datadenascimento"]
+    ministerio = request.form["ministerio"]
+    
+    if senha == confirma_senha:
+        exist_user = Membro.find_user_by_email(email=email)
+        if exist_user == None:
+            newUser = Membro(nome, email, senha)
+            newUser.set_password(senha)
+            newUser.create_user()
+            return make_response(redirect("/login"),302)
+        return make_response("<h1>Erro 400 - campos invalidos</h1>",400)
+    else:
+        return make_response("<h1>Senha não confirmada</h1>",400)
+
+@membros_router.post("/login")
+def login():
+    # Obtém os dados do formulário
+    data = {
+        'usuario': request.form.get("email"),  # assumindo que email é o campo usado para login
+        'senha': request.form.get("senha")
     }
+
+    # Chama a função de validação para verificar os dados de login
+    is_valid, error_message = validate_login_data(data)
+    if not is_valid:
+        flash(error_message, "Danger!")
+        return redirect("/login")
+
+    user = Membro.query.filter_by(email=data['usuario']).first()
+    if user is not None and user.check_password(data['senha']):
+        login_user(user, remember=True)
+        return redirect("/")
+
+    flash("Email ou senha inválidos", "Danger!")
+    return redirect("/login")
+
+@membros_router.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
+
+
