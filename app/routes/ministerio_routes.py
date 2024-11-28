c333dc3a5d0463f28bf82a22a5a6998ce52ebaea
Yan <yangarridodev@gmail.com>
2024-11-28 15:19:28 -0300

    ministerio funcionando

--- app/routes/ministerio_routes.py
+++ app/routes/ministerio_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import login_required
 from models.ministerio import Ministerio
 from configs.extensions import db
@@ -10,28 +10,40 @@ ministerios_router = Blueprint("ministerio_router", __name__, template_folder="t
 # Rota para listar todos os ministérios
 @ministerios_router.get("/ministerio")
 @login_required
-def cadastro_ministerio():
-    membros = db.session.execute(text("SELECT id_membro, nome FROM Membros")).fetchall()
-    print(membros)
-    return render_template('ministerioNOVO.html', membros=membros)
+def listar_ministerios():
+    # Obtendo ministérios do banco de dados
+    ministerios = Ministerio.query.all() 
+    
+    # Renderizando o template correto
+    return render_template("lista_ministerio.html", ministerios=ministerios)
+
+
+
+@ministerios_router.get("/ministerio/cadastro")
+@login_required
+def entrar_cadastro():
+    return render_template('ministerioNOVO.html')
 
 
 # Rota para criar um novo ministério
-@ministerios_router.post("/ministerio")
+@ministerios_router.post("/ministerio/cadastro")
 @login_required
 def create_ministerio():
     # Usando request.form para pegar os dados do formulário
     nome = request.form.get("nome")
     descricao = request.form.get("descricao")
-    id_membro = request.form.get("id_membro")
+    lider = request.form.get("lider")
     
     # Criando o novo ministério
     novo_ministerio = Ministerio(
         nome=nome,
         descricao=descricao,
-        id_membro=id_membro
+        lider=lider,
     )
 
     db.session.add(novo_ministerio)
     db.session.commit()
+
+    return redirect(url_for('ministerio_router.listar_ministerios'))
+
 
