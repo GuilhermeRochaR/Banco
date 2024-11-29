d7f6e049a97a5bfdf7e386e54c6eb7b02695bd68
Yan <121405668+YanGarrido@users.noreply.github.com>
2024-11-29 00:19:21 -0300

    Update Gatilhos.sql

--- Gatilhos.sql
+++ Gatilhos.sql
@@ -95,3 +95,18 @@ END //
 DELIMITER ;
 
 SHOW TRIGGERS FROM Igreja
+
+--Script de verificar email duplicado
+DELIMITER //
+
+CREATE TRIGGER verificaremaildupli
+BEFORE INSERT ON Membros
+FOR EACH ROW
+BEGIN
+    -- Verifica se o e-mail já existe na tabela Membros
+    IF EXISTS (SELECT 1 FROM Membros WHERE email = NEW.email) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'E-mail já está registrado!';
+    END IF;
+END //
+
+DELIMITER ;
